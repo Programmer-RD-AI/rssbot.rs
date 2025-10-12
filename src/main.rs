0eb9437a7efc2c36780ef155ca777824261becbe
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-10-12 23:07:03 +0530

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use hyper::rt::Executor;
 use hyper::server::conn::http2;
 use rssbot::utils::get_ip_split;
-use std::net::{Ipv4Addr, SocketAddr};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
 const DEFAULT_IP: &str = "127.0.0.1";
-const DEFAULT_PORT: i32 = 3000;
+const DEFAULT_PORT: u16 = 3000;
 
 #[derive(Clone)]
 pub struct TokioExecutor;
@@ -23,16 +23,16 @@ where
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    const DEFAULT_IP_OWNED: String = DEFAULT_IP.to_owned();
-    let ip_str: &str = std::env::var("IP_ADDRESS")
-        .unwrap_or(DEFAULT_IP_OWNED)
-        .as_str();
-    let port: i32 = std::env::var("PORT").unwrap_or(DEFAULT_PORT);
+    let ip_str: &str = std::env::var("IP_ADDRESS").as_deref().unwrap_or(DEFAULT_IP);
+    let port: u16 = std::env::var("PORT")
+        .unwrap()
+        .parse()
+        .unwrap_or(DEFAULT_PORT);
     let ip: Ipv4Addr = get_ip_split(ip_str);
-    let addr = SocketAddr::from((ip, port));
+    let addr = SocketAddr::from(SocketAddrV4::new(ip, port));
 }
 
-#[cfg(not(feature = "server"))]
-fn main() {
-    println!("Build without `--features server` — no server started.");
-}
+// #[cfg(not(feature = "server"))]
+// fn main() {
+//     println!("Build without `--features server` — no server started.");
+// }
